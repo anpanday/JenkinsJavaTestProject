1aecb6bb2056bc4b17c0af642f7fceed74a63b6c
anpanday <apanday@tmns.com>
2017-04-04 12:26:18 +0200

    add jenkinsfirstjavaproject

---
+++ JenkinsJavaProject.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+
+class JenkinsJavaProject
+	{
+		public static void main(String args[])
+
+			{
+				int x,y,z;
+				//System.out.println("Enter the integers");
+				//Scanner in = new Scanner(System.in);
+				//x = in.nextInt();
+				//y = in.nextInt();
+				x = 12;
+				y = 14;
+				z = x + y;
+				System.out.println("Sum of integers = " +z);
+			}
+	}
